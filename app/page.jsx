552b1669fa345db9e08c13ef47fdc82f72a30eae
Rafael Jimenez <125316019+rjmoreno2022@users.noreply.github.com>
2023-08-05 16:08:04 -0400

    Users list and navigation with next Router

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,21 @@
-function IndexPage() {
+import Users from "@/components/Users";
+
+async function fecthUsers() {
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  return data.data;
+}
+
+async function IndexPage() {
+  const users = await fecthUsers();
   return (
     <div>
       <h1>Hello World</h1>
+      <div>
+        <Users users={users}></Users>
+      </div>
     </div>
-  )
+  );
 }
 
 export default IndexPage
---
+++ app/users/[id]/page.jsx
@@ -0,0 +1,9 @@
+function UsersPage() {
+  return (
+    <div>
+      <h1>Users Details</h1>
+    </div>
+  )
+}
+
+export default UsersPage
