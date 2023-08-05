1ea0cf92435b523f97a46b857f392dc80db0c518
Rafael Jimenez <125316019+rjmoreno2022@users.noreply.github.com>
2023-08-05 12:18:05 -0400

    Navigations into other pages as About and Services

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+//"rfce" : atajo para crear componentes de react
+//"lorem100" : atajo para crear un texto de prueba con longitud de 100 palabras.
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut officia expedita hic, nihil ipsa iure ipsum fuga magni blanditiis similique soluta deserunt incidunt autem eaque eligendi a labore molestiae facilis alias nesciunt pariatur, vel doloribus quaerat maiores. Animi magnam eligendi alias quis commodi facere tempore non! Nihil reprehenderit culpa quidem. Mollitia amet tenetur provident nostrum pariatur quas eius tempora, similique cumque quod est, itaque dolor sed, illo accusantium sunt! Tempore autem et dignissimos. Labore quia aspernatur asperiores sit, cum illum vel minus quasi natus, doloribus nisi! Sint labore voluptatibus aspernatur quasi! Fuga sit ab, distinctio asperiores voluptate veniam magnam est.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,11 @@
+
+function ServicesPage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum nemo quis optio commodi magni veritatis facere? Beatae, necessitatibus corporis reprehenderit consectetur id in? Voluptates minima laudantium sit eum deserunt, ipsa, ipsum, quia assumenda asperiores ratione a quasi reprehenderit? Nemo officia repudiandae soluta molestiae sint modi atque reiciendis in eaque assumenda?</p>
+    </div>
+  )
+}
+
+export default ServicesPage
